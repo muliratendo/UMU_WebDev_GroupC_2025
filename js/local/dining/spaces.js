95c50889d93eee5639f543146fe812ba8b5a3ffb
KUYU <agabakakuru947@gmail.com>
2025-11-30 22:56:29 +0300

    fixed the links for dining

---
+++ js/local/dining/spaces.js
@@ -0,0 +1,19 @@
+const userName = document.getElementById('name');
+const email = document.getElementById('email');
+const date = document.getElementById('date');
+const time = document.getElementById('time');
+const form = document.getElementById('form');
+const errorElement = document.getElementById('error')
+
+
+form.addEventListener('submit',(e)=>{
+    let messages = []
+if (userName.vaule === '' || userName.vaule == null){
+    messages.push('Name is required')
+}
+ if (messages.lenght >0) {
+     e.preventDefault()
+     errorElement.innerText = messages.join(',')
+ }
+   
+})
